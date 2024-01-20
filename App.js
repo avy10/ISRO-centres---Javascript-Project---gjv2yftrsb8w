a97ff3e426632b1ef4a1326338cad9edccf9b3b2
Abhishek Kumar <avykr07@gmail.com>
2024-01-20 19:06:12 +0530

    created map and website data

--- App.js
+++ App.js
@@ -1,1 +1,175 @@
-
+const data = {
+    1 : {
+        map : "https://maps.app.goo.gl/mXNtzR4TrRTRkLii7",
+        website : "https://www.scl.gov.in/"
+    },
+    2 : {
+        map : "https://maps.app.goo.gl/WuQ4WxGiLn5kGi487",
+        website : "https://www.nrsc.gov.in/"
+    },
+    3 : {
+        map : "https://maps.app.goo.gl/6dR54hTpgek6F3jJ6",
+        website : "https://www.prl.res.in/prl-eng/division/usob"
+    },
+    4 : {
+        map : "https://maps.app.goo.gl/SEsaoY2VQd3nQteS8",
+        website : "https://www.sac.gov.in/"
+    },
+    5 : {
+        map : "https://maps.app.goo.gl/BokqJTkF1RDzcmyV6",
+        website : "https://www.prl.res.in/prl-eng/"
+    },
+    6 : {
+        map : "https://maps.app.goo.gl/yc1C2wb7t6vkkoym7",
+        website : "https://www.indiascienceandtechnology.gov.in/organisations/ministry-and-departments/department-space-isro/development-and-educational-communication-unit-decu-ahmedabad"
+    },
+    7 : {
+        map : "https://maps.app.goo.gl/ZH4vstvkvXfSwoqJA",
+        website : "https://www.prl.res.in/~miro/"
+    },
+    8 : {
+        map : "https://maps.app.goo.gl/uL9KRNKFYwgrVWPS9",
+        website : "https://www.mcf.gov.in/website/"
+    },
+    9 : {
+        map : "https://maps.app.goo.gl/HiEF8VYgwfnjXRCP7",
+        website : "https://www.isro.gov.in/"
+    },
+    10 : {
+        map : "https://maps.app.goo.gl/kaeNk5KqiMrzKF6FA",
+        website : "https://www.istrac.gov.in/"
+    },
+    12 : {
+        map : "https://maps.app.goo.gl/R3BGp58QRnvf47Bg7",
+        website : "https://www.mcf.gov.in/website/"
+    },
+    13 : {
+        map : "https://maps.app.goo.gl/qWfX7wxPQjdgmPAG7",
+        website : "https://www.isro.gov.in/VSSC.html"
+    },
+    14 : {
+        map : "https://maps.app.goo.gl/RVyyugYFebV6G2oe8",
+        website : "https://www.isro.gov.in/"
+    },
+    15 : {
+        map : "https://maps.app.goo.gl/RVyyugYFebV6G2oe8",
+        website : "https://www.isro.gov.in/"
+    },
+    16 : {
+        map : "https://maps.app.goo.gl/RVyyugYFebV6G2oe8",
+        website : "https://www.isro.gov.in/"
+    },
+    17 : {
+        map : "https://maps.app.goo.gl/RVyyugYFebV6G2oe8",
+        website : "https://www.vssc.gov.in/VSSC/"
+    },
+    18 : {
+        map : "https://maps.app.goo.gl/RVyyugYFebV6G2oe8",
+        website : "https://www.isro.gov.in/"
+    },
+    19 : {
+        map : "https://maps.app.goo.gl/LNYWB3r1prDnWX1Y9",
+        website : "https://www.antrix.co.in/"
+    },
+    20 : {
+        map : "https://maps.app.goo.gl/3b5VnnE2z4pmqUPVA",
+        website : "https://www.ursc.gov.in/"
+    },
+    21 : {
+        map : "https://maps.app.goo.gl/8LBbE81q5pn2nPc89",
+        website : "https://www.isro.gov.in/LEOS.html"
+    },
+    22 : {
+        map : "https://maps.app.goo.gl/2pqwddRhzBB8m4bUA",
+        website : "https://www.istrac.gov.in/"
+    },
+    23 : {
+        map : "https://maps.app.goo.gl/j6ENsmfLRfyj1DqL6",
+        website : "https://www.nrsc.gov.in/"
+    },
+    24 : {
+        map : "https://maps.app.goo.gl/Aqchbme3vcF54TWD9",
+        website : "https://www.lpsc.gov.in/"
+    },
+    25 : {
+        map : "https://maps.app.goo.gl/xQ8EHdecwyg6M8bJ8",
+        website : "https://www.hsfc.gov.in/"
+    },
+    26 : {
+        map : "https://maps.app.goo.gl/wvNLDX9ii33C52ra7",
+        website : "https://www.nsilindia.co.in/"
+    },
+    27 : {
+        map : "https://maps.app.goo.gl/L1JbtM6WZbKkSxie7",
+        website : "https://www.nsilindia.co.in/"
+    },
+    28 : {
+        map : "https://maps.app.goo.gl/L1JbtM6WZbKkSxie7",
+        website : "https://www.isro.gov.in/"
+    },
+    29 : {
+        map : "https://maps.app.goo.gl/XiTCq4WcnyVk25GZ9",
+        website : "https://www.sac.gov.in/"
+    },
+    30 : {
+        map : "https://maps.app.goo.gl/yE3M6fDgdBGoFM6GA",
+        website : "https://www.iirs.gov.in/"
+    },
+    31 : {
+        map : "https://maps.app.goo.gl/Bjv4TbDdRBQX3RPr5",
+        website : "https://cssteapun.org/"
+    },
+    32 : {
+        map : "https://maps.app.goo.gl/DFqGBxDaKR2WDMLD7",
+        website : "https://www.istrac.gov.in/"
+    },
+    33 : {
+        map : "https://maps.app.goo.gl/VjKtEknZ1JBVitit6",
+        website : "https://www.nrsc.gov.in/"
+    },
+    34 : {
+        map : "https://maps.app.goo.gl/cMybVw65pTU6LP1j7",
+        website : "https://nesac.gov.in/"
+    },
+    35 : {
+        map : "https://maps.app.goo.gl/2XnkKTEFwBT4W6KU6",
+        website : "https://www.nrsc.gov.in/"
+    },
+    36 : {
+        map : "https://maps.app.goo.gl/rsCH9uh4hgsdAv7Y9",
+        website : "https://www.nrsc.gov.in/"
+    },
+    37 : {
+        map : "https://maps.app.goo.gl/fy5rAZps2M5cdZdJA",
+        website : "https://www.narl.gov.in/"
+    },
+    38 : {
+        map : "https://maps.app.goo.gl/5w21GnwcbeUTjth7A",
+        website : "https://www.istrac.gov.in/"
+    },
+    39 : {
+        map : "https://maps.app.goo.gl/xqGPtCap8PNS2Qhy7",
+        website : "https://www.shar.gov.in/sdscshar/index.jsp"
+    },
+    40 : {
+        map : "https://maps.app.goo.gl/exav1ExA7boFiXay6",
+        website : "https://www.iprc.gov.in/"
+    },
+    41 : {
+        map : "https://maps.app.goo.gl/Z2od41RbvTjk3HWXA",
+        website : "https://www.vssc.gov.in/"
+    },
+    42 : {
+        map : "https://maps.app.goo.gl/3TiiFQNPxU7FxyBYA",
+        website : "https://www.lpsc.gov.in/"
+    },
+    43 : {
+        map : "https://maps.app.goo.gl/vHdxMhaqjExFWoqo9",
+        website : "https://www.isro.gov.in/IISU.html"
+    },
+    44 : {
+        map : "https://maps.app.goo.gl/fEXwL4baU3PkNecW8",
+        website : "https://www.iist.ac.in/"
+    },
+    
+}
